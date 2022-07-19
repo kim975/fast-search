2b9c5846ca9e57017d53eddb122a539b2f69950d
kim <scilo200@gmail.com>
2022-07-19 21:05:52 +0900

    #2 -스프링 부트 기본 파일
    스프링 부트 기본 파일 추가

---
+++ src/main/java/com/fast/search/FastSearchApplication.java
@@ -0,0 +1,13 @@
+package com.fast.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fast/search/FastSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
